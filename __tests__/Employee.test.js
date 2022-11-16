023a018b32cc0d1d69df7a9170d1eb8c3a53d59b
Seb <samm1497@hotmail.com>
2022-11-16 20:10:39 +0000

    employee tests are complete

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -11,6 +11,25 @@ test("creates a new Employee Object", () => {
 })
 
 
+//test getName Func
+test("check if the getName returns correct data", () => {
+    const employee = new Employee("Zelda", 123, "zelda@mail.com");
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+})
 
 
+//test getID func
+test("check if the getId Method returns correct data", () => {
+    const employee = new Employee("Zelda", 123, "zelda@mail.com");
 
+    expect(employee.getId()).toEqual(expect.any(Number));
+})
+
+
+//test getID func
+test("check if the getEmail method returns correct data", () => {
+    const employee = new Employee("Zelda", 123, "zelda@mail.com");
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+})
